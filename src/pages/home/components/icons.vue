210e9653609b2cda763e43120c03aa7e1de8581e
Jackson <1090359802@qq.com>
2019-10-17 15:43:07 +0800

    add icons

---
+++ src/pages/home/components/icons.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="icons">
+        <swiper>
+            <swiper-slide v-for="(page, index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-imgcontent" :src="item.imgUrl" alt="">
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconsList: [{
+        id: '0001',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/hotel.png',
+        desc: '酒店'
+      }, {
+        id: '0002',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/flight.png',
+        desc: '机票'
+      }, {
+        id: '0003',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/package.png',
+        desc: '度假'
+      }, {
+        id: '0004',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/train.png',
+        desc: '飞机票'
+      }, {
+        id: '0005',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/innBnb.png',
+        desc: '民宿客栈'
+      }, {
+        id: '0006',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/bargainflight.png',
+        desc: '低价机票'
+      }, {
+        id: '0007',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/group.png',
+        desc: '特惠酒店'
+      }, {
+        id: '0008',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/haiwai.png',
+        desc: '海外酒店'
+      }, {
+        id: '0009',
+        imgUrl: '//s.qunarzz.com/homenode/images/touchheader/piao.png',
+        desc: '景点门票'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconsList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    @import '~styles/mixins.styl'
+    .icons >>> .swiper-container
+        height 0
+        padding-bottom 50%
+    .icon
+        position relative
+        overflow hidden
+        float left
+        width 25%
+        height 0
+        padding-bottom 25%
+        .icon-img
+            position absolute
+            top 0
+            left 0
+            right 0
+            bottom 22px
+            box-sizing border-box
+            padding 5px
+            .icon-imgcontent
+                display block
+                margin 0 auto
+                height 100%
+        .icon-desc
+            position absolute
+            left 0
+            right 0
+            bottom 0
+            height 22px
+            line-height 22px
+            text-align center
+            color $darkTextColor
+            ellipsis()
+</style>
